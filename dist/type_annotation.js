f991fcbe36457045c5393507077a98db576cbba4
RyoHatano <ryohatano0809@gmail.com>
2023-06-24 12:38:39 +0900

    Inline type annotation

---
+++ dist/type_annotation.js
@@ -0,0 +1,4 @@
+"use strict";
+let human;
+human = { name: "Jack", age: 20 };
+console.log(`${human.name} is ${human.age} years old. `);
---
+++ src/type_annotation.ts
@@ -0,0 +1,5 @@
+// インラインの型注釈
+let human: { name: string; age: number };
+human = { name: "Jack", age: 20 };
+
+console.log(`${human.name} is ${human.age} years old. `);
