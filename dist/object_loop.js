9e5b6d9b62437825d33302e52e813d6cfcc3f4dc
RyoHatano <ryohatano0809@gmail.com>
2023-06-26 00:29:34 +0900

    object loop

---
+++ dist/object_loop.js
@@ -0,0 +1,20 @@
+"use strict";
+const foo = { a: 1, b: 2, c: 3 };
+for (const prop in foo) {
+    console.log(prop, foo[prop]);
+}
+console.log("--------------------");
+const example = { a: 4, b: 5, c: 6 };
+for (const [key, value] of Object.entries(example)) {
+    console.log(key, value);
+}
+console.log("--------------------");
+const example2 = { a: 7, b: 8, c: 9 };
+for (const key of Object.keys(example2)) {
+    console.log(key);
+}
+console.log("--------------------");
+const example3 = { a: 10, b: 11, c: 12 };
+for (const value of Object.values(example3)) {
+    console.log(value);
+}
