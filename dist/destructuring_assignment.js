213147b523896b60ff7441c4b9467cf269918438
RyoHatano <ryohatano0809@gmail.com>
2023-06-26 00:12:54 +0900

    destructuring_assignment

---
+++ dist/destructuring_assignment.js
@@ -0,0 +1,14 @@
+"use strict";
+const item = { price: 100 };
+const { price } = item; // const price = item.price;と同じ意味
+console.log(price);
+const continent = {
+    name: "日本",
+    jp: {
+        capital: "東京",
+        population: 10000000,
+    },
+};
+const { jp: { capital, population }, } = continent;
+console.log(capital); // const capital = continent.jp.capital;と同じ意味
+console.log(population); // const population = continent.jp.population;と同じ意味
