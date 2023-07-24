c7a2205541196ea0715773762643d3cbf69d0ed5
RyoHatano <ryohatano0809@gmail.com>
2023-07-24 11:51:25 +0900

    optional chain

---
+++ dist/optional_chain.js
@@ -0,0 +1,4 @@
+"use strict";
+const increment = (n) => n + 1;
+const result = increment === null || increment === void 0 ? void 0 : increment(1);
+console.log(result);
---
+++ src/optional_chain.ts
@@ -0,0 +1,3 @@
+const increment = (n:number) => n + 1;
+const result = increment?.(1);
+console.log(result);
